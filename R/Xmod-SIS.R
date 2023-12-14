86fce99f0b5a4efe9eb6e5bd5a6c185dfdd4f72c
David Smith <smitdave@gmail.com>
2023-12-14 15:07:35 -0800

    basic plotting moved to exDE

--- R/Xmod-SIS.R
+++ R/Xmod-SIS.R
@@ -1,37 +1,3 @@
-#' Basic plotting: plot the density of infected humans for an SIS model
-#'
-#' @inheritParams plot_X
-#' @export
-plot_X.SIS = function(model, clrs="black", stable=FALSE, add=FALSE){
-  vars = if(stable==TRUE){
-    model$outputs$stable_orbits
-  }else{
-    model$outputs$orbits
-  }
-
-  if(add==FALSE) with(vars$XH,
-      plot(time, 0*time, type = "n", ylim = c(0, max(H)),
-           ylab = "# Infected", xlab = "Time"))
-
-  lines_X(vars$XH, model, clrs)
-}
-
-#' Add the orbits for the SIS model to a plot for models of human infection and immunity
-#'
-#' @inheritParams lines_X
-#'
-#' @export
-lines_X.SIS = function(XH, model, clrs="black"){
-  with(XH,{
-    if(model$nStrata==1) lines(time, X, col=clrs)
-    if(model$nStrata>1){
-      if (length(clrs)==1) clrs=rep(clrs, model$nStrata)
-      for(i in 1:model$nStrata){
-        lines(time, X[,i], col=clrs[i])
-      }
-    }
-})}
-
 #' Split a stratum into two strata, assigning a fraction `p` a new biting weight that is multiplied by a factor `fac` compared with the old one. The biting weight for the remaining `1-p` gets a new factor `1/fac`
 #'
 #' @inheritParams split_stratum_by_biting
